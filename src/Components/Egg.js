7acf7442af45f3cb7dff78925b147dce4c8e4045
juliamanska <julia.ma@onet.com.pl>
2023-12-21 12:06:02 +0100

    Build Egg component with basic structure

---
+++ src/Components/Egg.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Egg = () => {
+  return (
+    <div className="egg">
+      <div className="albumen">
+        Albumen
+        <div className="yolk">Yolk</div>
+      </div>
+    </div>
+  );
+};
+
+export default Egg;
