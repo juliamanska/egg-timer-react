5bb56a24eb271f77bd0bfd5d09bd8d3cbf60a1e8
juliamanska <julia.ma@onet.com.pl>
2023-12-22 13:31:05 +0100

    Build timer with boiling time display

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTime } from "./CookingContext";
 
 const Timer = () => {
   const displayTime = useTime();
+  const [minutes, setMinutes] = useState(displayTime);
+  const [seconds, setSeconds] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
 
-  return <div> {displayTime}</div>;
+  useEffect(() => {
+    setMinutes(displayTime);
+  }, [displayTime]);
+
+  const countTime = () => {
+    const newInterval = setInterval(() => {
+      setSeconds((prevSeconds) => {
+        if (prevSeconds === 60) {
+          setMinutes((prevMinutes) => prevMinutes - 1);
+        }
+        if (prevSeconds === 0) {
+          setMinutes((prevMinutes) => {
+            if (prevMinutes === 0) {
+              clearInterval(newInterval);
+              setIntervalId(null);
+              return 0;
+            }
+            return prevMinutes;
+          });
+          return 60;
+        }
+        return prevSeconds - 1;
+      });
+    }, 1000);
+    setIntervalId(newInterval);
+  };
+
+  const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+  const formattedSeconds =
+    seconds < 10 ? "0" + seconds : seconds === 60 ? "00" : seconds;
+
+  const handleStartClick = () => {
+    if (!intervalId) {
+      countTime();
+    }
+  };
+
+  return (
+    <div>
+      <h2>
+        {formattedMinutes}:{formattedSeconds}
+      </h2>
+      <button onClick={handleStartClick}>Start</button>
+    </div>
+  );
 };
 
 export default Timer;
